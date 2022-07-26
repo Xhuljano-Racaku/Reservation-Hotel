c3b68b529f4500ea5971dcedb6338a781aa36fb1
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2022-07-26 17:04:54 -0400

    Added Customer
    Added customer bean, service, repository, and controller. Now the database is connected with Spring boot

--- Backend/Hotel-Reservation/src/main/java/com/skillstrom/hotelreservation/beans/Customer.java
+++ Backend/Hotel-Reservation/src/main/java/com/skillstrom/hotelreservation/beans/Customer.java
@@ -1,5 +1,75 @@
 package com.skillstrom.hotelreservation.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "customer")
 public class Customer {
 	
+	@Id
+	@Column(name="customer_id")
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int customerId;
+	@Column(name="first_name")
+	private String firstName;
+	@Column(name="last_name")
+	private String lastName;
+	@Column(name="phone")
+	private String phone;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Customer [customerId=" + customerId + ", firstName=" + firstName + ", lastName=" + lastName + ", phone="
+				+ phone + "]";
+	}
+
+	public Customer() {
+		super();
+	}
+
+	public Customer(String firstName, String lastName, String phone) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
---
+++ Backend/Hotel-Reservation/src/main/java/com/skillstrom/hotelreservation/services/CustomerService.java
@@ -0,0 +1,29 @@
+package com.skillstrom.hotelreservation.services;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skillstrom.hotelreservation.beans.Customer;
+import com.skillstrom.hotelreservation.repository.CustomerRepository;
+
+@Service
+public class CustomerService {
+	
+	@Autowired
+	private CustomerRepository repository;
+	
+	public CustomerService() {
+		System.out.println("Service created!");
+	}
+	
+	public List<Customer> findAll() {
+		return repository.findAll();
+	}
+	
+	public Customer save(Customer customer) {
+		return repository.save(customer);
+	}
+}
