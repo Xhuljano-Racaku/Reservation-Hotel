109d2fb447c64a46399501d47ec59b40aa2d6758
Brian Rodriguez <brian.epsi@gmail.com>
2022-07-27 17:07:40 -0400

    added header component added search from component started working on room card component

--- Frontend/Hotel-Reservation/src/app/app.module.ts
+++ Frontend/Hotel-Reservation/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import {FormsModule} from "@angular/forms";
+import { RoomCardComponent } from './room-card/room-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    RoomCardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
