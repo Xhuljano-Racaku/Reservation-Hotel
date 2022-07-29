5fb1b4e27c9722049ed49a5d92526668ae648485
DominicB06 <72121721+DominicB06@users.noreply.github.com>
2022-07-29 10:14:27 -0500

    Added search.component

--- Frontend/Hotel-Reservation/src/app/search/search.component.ts
+++ Frontend/Hotel-Reservation/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Room } from '../model/Room';
+import { RoomApiService } from '../room-api.service';
 
 @Component({
   selector: 'app-search',
@@ -7,17 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  tier :String = "";
+  tier :string = "";
   startDate :Date = new Date();
   endDate :Date = new Date();
   numberOfBeds :number = 0;
   minPrice :number = 0;
   maxPrice :number = 0;
 
+  roomsByAvailability: Array<Room> = []
+  roomsByPrice: Array<Room> = []
+  roomsByBeds: Array<Room> = []
+  roomsByTier: Array<Room> = []
 
-  constructor() { }
+  searchedRooms: Array<Room> = []
+
+  roomApi: RoomApiService
+
+  constructor(roomApi: RoomApiService) {
+    this.roomApi = roomApi
+   }
 
   ngOnInit(): void {
+  }
+
+  search(){
+
+    this.roomApi.findByAvailablity(this.startDate.toString(), this.endDate.toString()).subscribe(resp => {
+      this.roomsByAvailability = resp
+    })
+
+    this.roomApi.findByBeds(this.numberOfBeds).subscribe(resp => {
+      this.roomsByBeds = resp
+    })
+
+    this.roomApi.findByPrice(this.minPrice, this.maxPrice).subscribe(resp => {
+      this.roomsByPrice = resp
+    })
+
+    this.roomApi.findByTier(this.tier).subscribe(resp => {
+      this.roomsByTier = resp
+    })
+
+    this.searchedRooms = this.roomsByAvailability
+    .filter(x => this.roomsByBeds.includes(x))
+    .filter(x => this.roomsByPrice.includes(x))
+    .filter(x => this.roomsByTier.includes(x))
+
   }
 
 }
