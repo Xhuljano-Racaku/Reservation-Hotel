6baddc83371ff34f73022ec63ac7a89088236e0d
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2022-07-29 11:58:46 -0400

    CrossOrigin
    added crossorigins for all 3 controllers

--- Backend/Hotel-Reservation/src/main/java/com/skillstrom/hotelreservation/controller/ReservationController.java
+++ Backend/Hotel-Reservation/src/main/java/com/skillstrom/hotelreservation/controller/ReservationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/reservation/")
 public class ReservationController {
