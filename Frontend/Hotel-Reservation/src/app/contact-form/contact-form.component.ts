6e3ac702ac7f14ce6ac5de5cd0c98fc1f35174eb
Xhuljano Racaku <racakuxhuljano@yahoo.com>
2022-08-03 23:25:59 -0400

    Added contact form
    Added contact form and set up and email api to receive the submission form. Also clicking the icon will bring you to home page, and contact button will bring you to contact form submission

--- Frontend/Hotel-Reservation/src/app/contact-form/contact-form.component.ts
+++ Frontend/Hotel-Reservation/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup= new FormGroup({});
+
+  constructor(private builder: FormBuilder, private router: Router, private contact: ContactService) { }
+
+  onSubmit(contactForm: any) {
+    console.log(contactForm)
+    this.contact.PostMessage(contactForm)
+    .subscribe(response => {
+    console.log(response)
+    this.contactForm.reset();
+        setTimeout(()=> {
+          this.router.navigate(['/']);
+          },30)
+    location.href = 'https://mailthis.to/confirm'
+    }, error => {
+    console.warn(error.responseText)
+    console.log({ error })
+    })
+    }
 
   ngOnInit(): void {
+      this.contactForm = this.builder.group({
+      'Fullname': new FormControl('', [Validators.required]),
+      'Email': new FormControl('', [Validators.required, Validators.email]),
+      'Comment': new FormControl('', [Validators.required])
+      })
   }
 
 }
